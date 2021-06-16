fecaeafb4be3d6370bff001b18d5160af0086b79
Doug Jones <DSJR1284@GMAIL.COM>
2021-06-16 19:51:30 -0400

    commit tree 7f87f82f8b388f4ea7ccc234665493ac827c7fc5

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+let superbowlWin = (record) => {
+    let result = record.find(record => record.result === "W")
+    return !!result ? result.year : undefined
+}
+
+// const superbowlWin = (record) => {
+//     let results = record.find(record => record === "W")
+//    if (results.result === "W"){
+//     return results  
+//    } else { (results.year) 
+//     return undefined
+//    }
+// }
